e5801f7489c30e35d3e385c37d79f203866b213d
gimontania <gimontania@gmail.com>
2026-09-16 04:47:51 -0300

    feat: agendamiento de citas, agenda del médico y cambio de estado

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { crearAppointment } from "../controllers/appointment.controller.js";
+import { crearAppointment, cambiarEstadoAppointment, } from "../controllers/appointment.controller.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 import { authorize } from "../middlewares/authorize.middleware.js";
 import { validateAppointment } from "../middlewares/validate-appointment.js";
@@ -7,6 +7,22 @@ import { validateAppointment } from "../middlewares/validate-appointment.js";
 const router = Router();
 
 //creamos una cita
-router.post("/", verifyToken, authorize("RECEPCIONISTA"), validateAppointment, crearAppointment);
+router.post(
+    "/", 
+    verifyToken, 
+    authorize("RECEPCIONISTA"), 
+    validateAppointment, 
+    crearAppointment
+);
+
+//actualizamos el estado de una cita
+router.patch(
+    "/:id/status",
+    verifyToken, 
+    authorize("MEDICO"),
+    cambiarEstadoAppointment
+);
+
+
 
 export default router;
